56e32055db8dc3bea406068a79477b78cb693bc9
Daffa Ilham Ramadan <daffailhamramadan127@gmail.com>
2022-01-11 14:10:36 +0700

    Refactoring, Move TextmeshPro class reference to Class UIManager

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -1,18 +1,11 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class UIManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public TextMeshProUGUI scoreText;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public TextMeshProUGUI highscoreText;
+
+    public TextMeshProUGUI healthText;
 }
