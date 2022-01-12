c11ba5b4e5e5215e0009a2718b3e2d53c65136c8
Daffa Ilham Ramadan <daffailhamramadan127@gmail.com>
2022-01-12 12:13:40 +0700

    Refactoring

--- Assets/Scripts/Circle/CircleTouch.cs
+++ Assets/Scripts/Circle/CircleTouch.cs
@@ -6,37 +6,36 @@ public class CircleTouch : MonoBehaviour
 
     private AudioSource audioSource;
 
-    public GameRule game;
+    private GameController gameController;
 
     private void Awake()
     {
         audioSource = GetComponent<AudioSource>();
 
         m_SpriteRenderer = GetComponent<SpriteRenderer>();
+
+        gameController = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameController>();
     }
 
     void Start()
     {
-        //Start with color "red"
         m_SpriteRenderer.color = Color.red;
 
     }
 
     void Update()
     {
-
-        //Change Color to green when touch (multi touch) the circle and add score +1 
         for (int i = 0; i < Input.touchCount; i++)
         {
             Touch touch = Input.GetTouch(i);
 
             RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(touch.position), Vector2.zero);
 
-            if (hit.collider != null && hit.transform == transform && touch.phase == TouchPhase.Began && m_SpriteRenderer.color == Color.red && game.isGameover != true && game.isPaused != true)
+            if (hit.collider != null && hit.transform == transform && touch.phase == TouchPhase.Began && m_SpriteRenderer.color == Color.red && gameController.gameState == GameController.GameState.Play) 
             { 
                 m_SpriteRenderer.color = Color.green;
 
-                game.score += 1;
+                gameController.score += 1;
 
                 audioSource.Play();
             }
